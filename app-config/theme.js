52df5cd4db14684b29049ea8dd04542451a09fea
‘full <verticalorigin@yahoo.com>
2022-07-15 20:05:11 +0100

    created app structure

---
+++ app-config/theme.js
@@ -0,0 +1,7 @@
+export const ColorScheme = {
+    primary: '#f9beda',
+    secondary: '#c30b64',
+    accent: '#610440',
+    errorLight: '#fcdcbf',
+    errorDark: '#f37c13',
+  }
---
+++ screens/loginScreen.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+function loginScreen() {
+    return <View>
+        <Text> Login Screen</Text>
+    </View>
+}
+
+export default loginScreen;
